0e6b8bdfd7b3f53ed295c036e6436cec9f92db7e
Isai Oliva <i.oliva@uniandes.edu.co>
2022-09-06 23:10:12 -0400

    primeras configuracione ORM

---
+++ src/modelo/automovil.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from .declarative_base import Base
+
+
+class Automovil(Base):
+    __tablename__ = 'automovil'
+
+    id = Column(Integer, primary_key=True)
+    marca = Column(String)
+    placa = Column(String)
+    Modelo = Column(Integer)
+    kilometraje = Column(Integer)
+    color = Column(String)
+    cilindraje = Column(String)
+    combutible = Column(String)
+    acciones = relationship('Accion', cascade='all, delete, delete-orphan')
+
+
