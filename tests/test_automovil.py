be2bef378eca800c4ba9b0554b250ce033be2df1
david <da.vargasl1@uniandes.edu.co>
2022-09-07 20:12:31 -0500

    feacture configure mock test Auto

--- tests/test_automovil.py
+++ tests/test_automovil.py
@@ -28,6 +28,6 @@ class AutomovilTestCase(unittest.TestCase):
         self.session.close()
 
     def test_crear_automovil_01(self):
-        self.logica.aniadir_mantenimiento("llanta", "cambiar presión de llantas")
-        #automovil = self.session.query(Automovil).filter(Automovil.placa == 'llanta').first()
-        self.assertEqual("llanta", "llanta")
+        self.logica.crear_auto("renault", "JXL539", 1970, 3200, "negro", 2000, "gasolina")
+        automovil = self.session.query(Auto).filter(Auto.placa == 'JXL539').first()
+        self.assertEqual(automovil.placa, "JXL539")
